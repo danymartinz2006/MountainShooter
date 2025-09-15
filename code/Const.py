3e3b92b9af924002fd0a44a9ae0d7294adca2b49
danymartinz2006 <danymartinz.2006@gmail.com>
2025-09-15 01:31:40 -0300

    Creating mediator pattern. enemies destruction

--- code/Const.py
+++ code/Const.py
@@ -16,9 +16,13 @@ ENTITY_SPEED = {
     'Level1Bg5': 5,
     'Level1Bg6': 6,
     'Player1': 3,
+    'Player1Shot': 1,
     'Player2': 3,
-    'Enemy1': 2,
+    'Player2Shot': 3,
+    'Enemy1': 1,
+    'Enemy1Shot': 3,
     'Enemy2': 1,
+    'Enemy2Shot': 3,
 }
 
 ENTITY_HEALTH = {
@@ -32,11 +36,18 @@ ENTITY_HEALTH = {
     'Player1': 300,
     'Player1Shot': 1,
     'Player2': 300,
-    'Player2Shot': 300,
+    'Player2Shot': 1,
     'Enemy1': 50,
     'Enemy1Shot': 1,
     'Enemy2': 60,
-    'Enemy2Shot': 300,
+    'Enemy2Shot': 2,
+}
+
+ENTITY_SHOT_DELAY = {
+    'Player1': 20,
+    'Player2': 15,
+    'Enemy1': 30,
+    'Enemy2': 40,
 }
 
 # M
@@ -50,13 +61,13 @@ MENU_OPTION = ('NEW GAME 1P',
 PLAYER_KEY_UP = {'Player1': pygame.K_UP,
                  'Player2': pygame.K_w}
 PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
-                 'Player2': pygame.K_s}
+                   'Player2': pygame.K_s}
 PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
-                 'Player2': pygame.K_a}
+                   'Player2': pygame.K_a}
 PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
                     'Player2': pygame.K_d}
-PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
-                    'Player2': pygame.K_RCTRL}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_SPACE,
+                    'Player2': pygame.K_LCTRL}
 
 # S
 SPAWN_TIME = 4000
--- code/EntityMediator.py
+++ code/EntityMediator.py
@@ -1,5 +1,8 @@
+from code.Const import WIN_WIDTH
 from code.Enemy import Enemy
+from code.EnemyShot import EnemyShot
 from code.Entity import Entity
+from code.PlayerShot import PlayerShot
 
 
 class EntityMediator:
@@ -7,7 +10,13 @@ class EntityMediator:
     @staticmethod
     def __verify_collision_window(ent: Entity):
         if isinstance(ent, Enemy):
-            if ent.health < 0:
+            if ent.rect.right <= 0:
+                ent.health = 0
+        if isinstance(ent, PlayerShot):
+            if ent.rect.left >= WIN_WIDTH:
+                ent.health = 0
+        if isinstance(ent, EnemyShot):
+            if ent.rect.right <= 0:
                 ent.health = 0
 
     @staticmethod
@@ -15,6 +24,7 @@ class EntityMediator:
         for i in range(len(entity_list)):
             test_entity = entity_list[i]
             EntityMediator.__verify_collision_window(test_entity)
+
 
     @staticmethod
     def verify_health(entity_list: list[Entity]):
